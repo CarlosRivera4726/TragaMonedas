36369edc152338471f341d426a62350fa0e4a639
Carlos Rivera <carlosrivera_-717@hotmail.com>
2023-03-03 20:20:14 -0500

    Clase Persona y Player
    añadida clase Persona y Player

---
+++ TragaMonedas/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TragaMonedas
+{
+    internal class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private string cartera;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Cartera { get => cartera; set => cartera = value; }
+
+        public Persona() { }
+        public Persona(string nombre, string apellido, string cartera)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Cartera = cartera;
+        }
+    }
+}
---
+++ TragaMonedas/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TragaMonedas
+{
+    internal class Player : Persona
+    {
+        private string monedas;
+        private bool estado;
+
+        public string Monedas { get => monedas; set => monedas = value; }
+        public bool Estado { get => estado; set => estado = value; }
+
+        public Player() { }
+        public Player(string monedas, bool estado)
+        {
+            Monedas = monedas;
+            Estado = estado;
+        }
+    }
+}
